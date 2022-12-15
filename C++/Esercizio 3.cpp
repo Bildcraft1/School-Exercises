a6cb67ce97f6ba14b9ba2894345233698d9b3992
WhiXard <bildcraft10000@gmail.com>
2022-12-15 10:03:54 +0100

    Exsercise 3

---
+++ C++/Esercizio 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int eta;
+
+    cout << "ESERCIZIO 5" << endl;
+
+    do {
+        cout << "Inserisci la tua eta' = ";
+        cin >> eta;
+    } while(eta <= 0);
+
+    cout << endl;
+
+    if (eta >= 18) {
+        cout << "Puoi avere la patente sia in italia che negli altri stati";
+    } else if(eta >= 16) {
+        cout << "Non puoi avere la patente ma in altri stati si";
+    };
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
