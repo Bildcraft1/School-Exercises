93539e464f8c8012890ae92ac972743a7582e70b
WhiXard <bildcraft10000@gmail.com>
2022-12-19 07:36:15 +0000

    For statement

---
+++ C++/For statement/Esercizio 1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int grandezza, input, max = 0, min = 10, media = 0;
+
+    cout << "Esercizio 1" << endl;
+    cout << "Inserire gradezza ciclo: ";
+    cin >> grandezza;
+
+    for (int n = 0; n < grandezza; n++)
+    {
+        cout << "Inserisci numero a posizione " << (n + 1) << " : ";
+        cin >> input;
+
+        if (input > max)
+        {
+            max = input;
+        };
+
+        if (input < min)
+        {
+            min = input;
+        };
+
+        media += input;
+    }
+
+    cout << "Media: " << (media / grandezza) << endl;
+    cout << "Max: " << max << endl;
+    cout << "Min: " << min << endl;
+
+    cout << endl;
+    return 0;
+}
