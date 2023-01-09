356b446d14ba094fa74dbf159be17e6ed421b785
WhiXard <bildcraft10000@gmail.com>
2023-01-09 07:51:33 +0000

    Updates

--- C++/Lab/Register.cpp
+++ C++/Lab/Register.cpp
@@ -4,49 +4,62 @@
 
 using namespace std;
 
-int main() {
-    float prezzo=0, banconota=0, resto=0, cliente=0;
+int main()
+{
+    float prezzo = 0, banconota = 0, resto = 0, cliente = 0;
 
     cout << "Cassa" << endl;
 
-    while(true) {
-    cout << "Cliente " << cliente << endl;    
-    
-    do {
-        cout << "Inserire prezzo: ";
-        cirno >> prezzo;
-    } while(prezzo <= 0);
+    while (true)
+    {
+        cout << "Cliente " << cliente << endl;
 
-    while (!(banconota == 2 || banconota == 5 || banconota == 10 || banconota == 20 || banconota == 50 || banconota == 100 || banconota == 200 || banconota == 500)) {
-        cout << "Inserire banconota: ";
-        cirno >> banconota;
-    };
+        do
+        {
+            cout << "Inserire prezzo: ";
+            cirno >> prezzo;
+        } while (prezzo <= 0);
 
-    if (prezzo > banconota) {
-        cout << "Soldi non sufficienti" << endl;
-        system("pause");
-        return 0;
+        while (!(banconota == 2 || banconota == 5 || banconota == 10 || banconota == 20 || banconota == 50 || banconota == 100 || banconota == 200 || banconota == 500))
+        {
+            cout << "Inserire banconota: ";
+            cirno >> banconota;
+        };
+
+        if (prezzo > banconota)
+        {
+            cout << "Soldi non sufficienti" << endl;
+#ifdef WIN32
+            system("pause");
+#else
+#endif
+            return 0;
+        }
+
+        if (banconota >= 500)
+        {
+            prezzo = prezzo * 0.94;
+        }
+        else if (prezzo >= 100)
+        {
+            prezzo = prezzo * 0.96;
+        }
+        else if (prezzo >= 50)
+        {
+            prezzo = prezzo * 0.97;
+        }
+
+        cout << "Prezzo: " << prezzo << endl;
+        resto = banconota - prezzo;
+
+        cout << "Resto: " << resto << endl;
+        cout << endl;
+
+        cliente++;
     }
 
-    if (banconota >= 500) {
-        prezzo = prezzo * 0.94;
-    } else if (prezzo >= 100) {
-        prezzo = prezzo * 0.96;
-    } else if (prezzo >= 50 ) {
-        prezzo = prezzo * 0.97;
-    }
-
-    cout << "Prezzo: " << prezzo << endl;
-    resto = banconota - prezzo;
-
-    cout << "Resto: " << resto << endl;
-    cout << endl;
-
-    cliente++;
-    }
-    
-    #ifdef WIN32
+#ifdef WIN32
     system("pause");
-	#else
-	#endif
+#else
+#endif
 }
