ffeb7cdf6be3aecd62ded9c2d75255177009a4bc
WhiXard <bildcraft10000@gmail.com>
2023-01-11 16:07:02 +0000

    Fixed

--- C++/Lab/PresistoCasa.cpp
+++ C++/Lab/PresistoCasa.cpp
@@ -5,33 +5,60 @@ using namespace std;
 
 int main()
 {
-    float prestito, rata, interesse, durata, prezzomese, totale=0;
+    float prestito, rata, interesse, durata, prezzomese, totale=0, presitobanca;
+    int anni=0;
 
     cout << "Prestito Casa" << endl;
 
     cout << "Inserisci il prestito: ";
     cin >> prestito;
+
+    while (prestito <= 0) {
+        cout << endl;
+        cout << "Attenzione: il prestito deve essere maggiore di 0" << endl;
+        cout << "Inserisci il prestito: ";
+        cin >> prestito;
+    }
+
     prestito = prestito*0.90;
+    cout << "Prestito considerato dalla banca: " << prestito << endl;
+    presitobanca = prestito;
 
     cout << "Inserisci l'interesse: ";
     cin >> interesse;
 
-    cout << "Inserisci la durata: ";
-    cin >> durata;
+    while (interesse <= 0) {
+        cout << endl;
+        cout << "Attenzione: l'interesse deve essere maggiore di 0" << endl;
+        cout << "Inserisci l'interesse: ";
+        cin >> interesse;
+    }
 
     cout << "Inserisci prezzo al mese: ";
     cin >> prezzomese;
 
-    for(int i=0; i<durata; i++)
-    {
-        rata = (prezzomese*12) + (prestito / 100 * interesse);
-        cout << "Rata " << i+1 << ": " << rata << endl;
-        prestito -= prezzomese*12;
-        cout << "Prestito: " << prestito << endl;
-        totale += rata;
+    while (prezzomese <= 0) {
+        cout << endl;
+        cout << "Attenzione: il prezzo mensile deve essere maggiore di 0" << endl;
+        cout << "Inserisci prezzo al mese: ";
+        cin >> prezzomese;
     }
 
-    cout << "Totale: " << totale << endl;
+    while(prestito > 0)
+    {
+        rata = (prezzomese*12) + (prestito / 100 * interesse);
+        cout << "Rata " << anni+1 << ": " << rata << endl;
+        prestito = prestito - (prezzomese*12);
+        if (prestito > 0) {
+            cout << "Prestito: " << prestito << endl;
+        }
+        totale += rata;
+        anni++;
+    }
+
+    cout << "Totale speso: " << totale << endl;
+    cout << "Totale speso in più: " << totale - presitobanca << endl;
+    cout << "Anni: " << anni << endl;
 
 #ifdef WIN32
     system("pause");
