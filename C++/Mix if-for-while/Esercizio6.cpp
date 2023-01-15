f2612a46c799dc4c83f28c442f421fb5cc72dc35
WhiXard (WSL) <bildcraft10000@gmail.com>
2023-01-15 14:58:00 +0100

    Exercise 6

--- C++/Mix if-for-while/Esercizio6.cpp
+++ C++/Mix if-for-while/Esercizio6.cpp
@@ -4,10 +4,28 @@
 using namespace std;
 
 int main() {
-    int i = 0;
-    while(i = 20) {
-        cout << rand()%11;
-        i++;
+    // Generate random numbers until the generated number is equal to the last one generated
+    int last = 0;
+    int current = 1;
+    int count = 0, aftercount = 0;
+
+    srand(time(NULL));
+    
+    while (current != last) {
+        last = current;
+	current = rand()%10 + 1;
+	count++;
+    	cout << current << " ";
+	if (current == last+1) {
+                aftercount++;
+        }
     }
+
+    cout << endl;
+    cout << "Numero di numeri generati: " << count << endl;
+    cout << "Numero di numeri generati dopo che il numero corrente e' stato uguale al precedente: " << aftercount << endl;
+
+
+    
     return 0;
 }
