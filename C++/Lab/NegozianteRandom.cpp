be836ec5015e207507f05ab7780e42b60cea24f7
WhiXard <bildcraft10000@gmail.com>
2023-01-19 08:35:29 +0000

    Update to use functions

--- C++/Lab/NegozianteRandom.cpp
+++ C++/Lab/NegozianteRandom.cpp
@@ -6,7 +6,70 @@
 
 using namespace std;
 
-ofstream fout("output.txt");
+float calcolaResto(float prezzo, float banconota)
+{
+    if (banconota >= 500)
+    {
+        prezzo = prezzo * 0.94;
+    }
+    else if (prezzo >= 100)
+    {
+        prezzo = prezzo * 0.96;
+    }
+    else if (prezzo >= 50)
+    {
+        prezzo = prezzo * 0.97;
+    }
+
+    return banconota - prezzo;
+}
+
+void tagliResto(float resto) {
+    // Cant use arrays 
+    int cinquecento = 0, duecento = 0, cento = 0, cinquanta = 0, venti = 0, dieci = 0, cinque = 0, due = 0, uno = 0;
+
+    while((int)resto > 0) {
+        if (resto >= 500) {
+            resto -= 500;
+            cinquecento++;
+        } else if (resto >= 200) {
+            resto -= 200;
+            duecento++;
+        } else if (resto >= 100) {
+            resto -= 100;
+            cento++;
+        } else if (resto >= 50) {
+            resto -= 50;
+            cinquanta++;
+        } else if (resto >= 20) {
+            resto -= 20;
+            venti++;
+        } else if (resto >= 10) {
+            resto -= 10;
+            dieci++;
+        } else if (resto >= 5) {
+            resto -= 5;
+            cinque++;
+        } else if (resto >= 2) {
+            resto -= 2;
+            due++;
+        } else if (resto >= 1) {
+            resto -= 1;
+            uno++;
+        }
+    }
+
+    cout << "Resto: " << endl;
+    cout << "500: " << cinquecento << endl;
+    cout << "200: " << duecento << endl;
+    cout << "100: " << cento << endl;
+    cout << "50: " << cinquanta << endl;
+    cout << "20: " << venti << endl;
+    cout << "10: " << dieci << endl;
+    cout << "5: " << cinque << endl;
+    cout << "2: " << due << endl;
+    cout << "1: " << uno << endl;
+}
 
 int main()
 {
@@ -28,28 +91,15 @@ int main()
         };
         cout << "Banconota: " << banconota << endl;
 
-        if (banconota >= 500)
-        {
-            prezzo = prezzo * 0.94;
-        }
-        else if (prezzo >= 100)
-        {
-            prezzo = prezzo * 0.96;
-        }
-        else if (prezzo >= 50)
-        {
-            prezzo = prezzo * 0.97;
-        }
-
-        cout << "Prezzo: " << prezzo << endl;
-        resto = banconota - (int)prezzo;
+        resto = calcolaResto(prezzo, banconota);
 
         cout << "Resto: " << resto << endl;
         cout << endl;
 
+        tagliResto(resto);
+
         cliente++;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        fout << cliente << " " << prezzo << " " << banconota << " " << resto << endl;
     }
 
 #ifdef WIN32
