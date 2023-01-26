78c7e87447fcd7c0bd907fa89e27a47f6af5fa38
WhiXard <bildcraft10000@gmail.com>
2023-01-26 10:44:45 +0000

    SwapVariables

---
+++ C++/Lab/SwapVariables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int a = 0, b = 0;
+    cout << "Inserisci il primo numero: ";
+    cin >> a;
+    cout << "Inserisci il secondo numero: ";
+    cin >> b;
+
+    cout << "Prima dello swap: " << a << " " << b << endl;
+    swap(a, b);
+    cout << "Dopo lo swap: " << a << " " << b << endl;
+
+    return 0;
+}
