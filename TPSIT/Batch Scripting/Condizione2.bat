e799750a585f113c3f4b6e055ae8a03b2c79a745
Bildcraft1 <bildcraft10000@gmail.com>
2023-02-28 19:07:00 +0100

    Update

--- TPSIT/Batch Scripting/Condizione2.bat
+++ TPSIT/Batch Scripting/Condizione2.bat
@@ -1,6 +1,11 @@
 @echo off
 set /p folder=Inserisci il percorso della cartella: 
 
+if not exist "%folder%" (
+    echo La cartella non esiste
+    goto more
+)
+
 REM Visualizza i file e i relativi attributi nella cartella selezionata
 dir %folder%
 
@@ -9,24 +14,29 @@ set /p action=Quale azione vuoi eseguire? (C)ancellare, (R)inominare o (S)postar
 
 IF /I "%action%"=="C" (
     REM Cancella i file selezionati
-    set /p ext=Inserisci l'estensione del file che vuoi cancellare (senza il punto): 
+    set /p ext="Inserisci l'estensione del file che vuoi cancellare (senza il punto) = "
     del %folder%\*.%ext%
+    goto more
 ) ELSE IF /I "%action%"=="R" (
     REM Rinomina i file selezionati
-    set /p ext=Inserisci l'estensione del file che vuoi rinominare (senza il punto): 
-    set /p oldname=Inserisci il nome del file che vuoi rinominare: 
-    set /p newname=Inserisci il nuovo nome del file: 
+    set /p ext="Inserisci l'estensione del file che vuoi rinominare (senza il punto) = "
+    set /p oldname="Inserisci il nome del file che vuoi rinominare = "
+    set /p newname="Inserisci il nuovo nome del file = "
     ren %folder%\%oldname%.%ext% %newname%.%ext%
+    goto more
 ) ELSE IF /I "%action%"=="S" (
     REM Sposta i file selezionati
-    set /p ext=Inserisci l'estensione del file che vuoi spostare (senza il punto): 
-    set /p oldpath=Inserisci il percorso completo del file che vuoi spostare: 
-    set /p newpath=Inserisci il percorso completo della nuova cartella di destinazione: 
+    set /p ext="Inserisci l'estensione del file che vuoi spostare (senza il punto) = "
+    set /p oldpath="Inserisci il percorso completo del file che vuoi spostare = "
+    set /p newpath="Inserisci il percorso completo della nuova cartella di destinazione = "
     move %folder%\%oldpath%.%ext% %newpath%
+    goto more
 )
 
+:more
+
 REM Chiede all'utente se vuole eseguire altre operazioni
-set /p more=Vuoi eseguire altre operazioni? (S)i o (N)o: 
+set /p more="Vuoi eseguire altre operazioni? (S)i o (N)o? "
 
 IF /I "%more%"=="S" (
     REM Esegue il file batch di nuovo
