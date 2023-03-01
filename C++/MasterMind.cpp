7b16219fc2e2da0bbd4f0b51eb8bf8416a71194a
Bildcraft1 <bildcraft10000@gmail.com>
2023-03-01 17:38:48 +0100

    Stuffs

--- C++/MasterMind.cpp
+++ C++/MasterMind.cpp
@@ -5,14 +5,20 @@
 
 using namespace std;
 
-random_device rd;
-mt19937 rng(rd());
+int menu()
+{
+    int choice;
+    cout << "1. Gioca" << endl;
+    cout << "2. Esci" << endl;
+    cout << "Scelta: ";
+    cout.flush();
+    cin >> choice;
+    return choice;
+}
 
 void codiceCasuale(string code[], int SIZE) {
-    uniform_int_distribution<int> xDist(0, 8);
-
     for (int i = 0; i < SIZE; i++) {
-        int random = xDist(rng);
+        int random = rand() % 9;
         switch (random) {
             case 0:
                 code[i] = "R";
@@ -60,38 +66,139 @@ bool checkCode(string code[], string answer[], int SIZE) {
     }
 }
 
+void checkPos(string code[], string answer[], int SIZE) {
+    int posGiusta = 0, posErrata = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (code[i] == answer[i])
+        {
+            posGiusta++;
+        }
+        else
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                if (code[i] == answer[j])
+                {
+                    posErrata++;
+                }
+            }
+        }
+    }
+
+    cout << "Colori in posizione giusta: " << posGiusta << endl;
+    cout << "Colori giusti ma in posizione sbagliata: " << posErrata << endl;
+}
+
+void stampaArray(string array[], int SIZE) {
+    for (int i = 0; i < SIZE; i++) {
+        cout << array[i] << " ";
+    }
+}
+
 void inserisciCodice(string answer[], int SIZE) {
+    cout << "Inserisci il codice: ";
     for (int i = 0; i < SIZE; i++) {
         cout << "Inserisci il " << i + 1 << "° colore: ";
         cin >> answer[i];
+
+        if (answer[i][0] >= 'a' && answer[i][0] <= 'z')
+        {
+            answer[i][0] -= 32;
+        }
     }
+
+    cout << endl;
+
+    cout << "Codice inserito: ";
+    cout.flush();
+    stampaArray(answer, SIZE);
 }
+
+void clearScreen()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void pause()
+{
+#ifdef _WIN32
+    system("pause");
+#else
+    system("read -p 'Press Enter to continue...' var");
+#endif
+}
+
+
 
 int main() {
     const int SIZE = 4;
     string code[SIZE];
     string answer[SIZE];
-    int maxTries = 10, tries = 0;
+    int maxTries = 10, tries = 0, input = 0;
     bool win = false;
+
+    srand(time(NULL));
+
+    cout << "Benvenuto in MasterMind!" << endl;
 
     codiceCasuale(code, SIZE);
 
-    for (int i = 0; i < SIZE; i++) {
-        cout << code[i] << " ";
-    }   
+    do
+    {
 
-    cout << endl;
+        input = menu();
 
-    while (tries < maxTries && !win) {
-        inserisciCodice(answer, SIZE);
-        if (checkCode(code, answer, SIZE)) {
-            cout << "Hai indovinato il codice!" << endl;
-            break;
-        } else {
-            cout << "Hai sbagliato il codice!" << endl;
-            tries++;
+        while (tries < maxTries && !win && input == 1)
+        {
+            clearScreen();
+
+            inserisciCodice(answer, SIZE);
+
+            if (checkCode(code, answer, SIZE))
+            {
+                win = true;
+            }
+            else
+            {
+                tries++;
+            }
+
+            checkPos(code, answer, SIZE);
+
+            if (!win)
+            {
+                cout << "Tentativi rimanenti: " << maxTries - tries << endl;
+                pause();
+            }
         }
-    }
+
+        if (win && input == 1)
+        {
+            cout << "Hai vinto!" << endl;
+            pause();
+        }
+        else if (tries == maxTries && input == 1)
+        {
+            cout << "Hai perso!" << endl;
+            cout << "La sequenza era: ";
+            stampaArray(code, SIZE);
+            pause();
+
+
+        }
+        
+        // Reset variabili
+        win = false;
+        tries = 0;
+        codiceCasuale(code, SIZE);
+        cout << endl;
+
+    } while (input != 2);
 
     return 0;
 }
